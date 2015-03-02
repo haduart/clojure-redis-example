e9b81907367a5b428eee1730f0aae658a35c1940
haduart <haduart@gmail.com>
2015-03-02 22:16:59 +0100

    Adding license to the project

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clojure-redis-example "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/haduart/clojure-redis-example"
+  :license {:name "BSD"
+            :url "http://www.opensource.org/licenses/BSD-3-Clause"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.taoensso/carmine "2.7.1"]
                  [clj-time "0.6.0"]
